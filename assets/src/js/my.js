c8669db4c100459833a604ebb4fe8a146e13e780
ES-Good <evgenshtanko@ya.ru>
2022-03-19 15:07:36 +0400

    'e'

--- assets/src/js/my.js
+++ assets/src/js/my.js
@@ -31,3 +31,37 @@ var cases = new Swiper(".cases", {
       prevEl: ".reviews-prev",
     }
   });
+
+
+let btnAudio = document.querySelectorAll('.btn-audio'); //кнопки воспроизведения
+let audio = document.querySelectorAll('.audio');  //скрытые аудио фалы, находятся в вёрстке
+
+
+for (let i = 0; i < btnAudio.length; i++) {
+  btnAudio[i].onclick = function () { //клик по кнопке
+
+    for (let k = 0; k < btnAudio.length; k++) {
+      if (i == k) { //проверка на какой кнопке произошло событие(на остальных удаляет кроме той на которой произошло событие)
+
+        if (btnAudio[i].classList.contains('btn-audio_active')) { // проверка на то если клик произошёл на уже играющем файле(ставит на паузу)
+
+          btnAudio[k].classList.remove('btn-audio_active');
+          audio[k].pause()
+
+        }else{ //просто воспроизводит если первое условие не сработало
+
+          btnAudio[i].classList.add('btn-audio_active');
+          audio[i].play()
+
+        }
+
+      }else{ //останавливает
+
+        btnAudio[k].classList.remove('btn-audio_active');
+        audio[k].pause()
+
+      }
+    }
+
+  }
+}
